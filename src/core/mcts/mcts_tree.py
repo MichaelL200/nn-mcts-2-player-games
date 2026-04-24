ee7ad87688500d7519b45f3ef8249679851ca56f
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2026-04-24 20:47:19 +0200

    refactor: get action size from game atribute

--- src/core/mcts/mcts_tree.py
+++ src/core/mcts/mcts_tree.py
@@ -111,8 +111,8 @@ class MCTSTree:
 
     def get_action_prob(self) -> np.ndarray:
         # Function for training
-        # If no model is provided, assume default action space size (1024)
-        action_size = self.model.model.action_size if self.model is not None else 1024
+        # If no model is provided, assume default action space size
+        action_size = self.model.model.action_size if self.model is not None else self.game.get_action_size()
         action_probs = np.zeros(action_size, dtype=np.float32)
 
         counts = [child.visit_count for child in self.root.children_nodes]
