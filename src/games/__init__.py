2a55df5e618e51a2079c25d7f5890c0413e55682
Piotr Lenczewski <piotrlenczewskipl@o2.pl>
2026-08-19 23:54:44 +0200

    feat: make chess available through __init__.py

--- src/games/__init__.py
+++ src/games/__init__.py
@@ -1,9 +1,15 @@
 from .checkers import Checkers, CheckersPlayer, CheckersUI, CheckersEncoder  # noqa
+from .chess import Chess, ChessPlayer, ChessEncoder  # noqa
 
 GAMES = {
     "checkers": {
         "simulation": Checkers,
         "ui": CheckersUI,
         "model_file": "checkers_alphazero_model.pt",
+    },
+    "chess": {
+        "simulation": Chess,
+        "ui": None,
+        "model_file": "chess_alphazero_model.pt",
     },
 }
